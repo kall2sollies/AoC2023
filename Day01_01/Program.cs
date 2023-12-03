5a393151f8b11631609ca64fd57512f7d1091e7d
Calendau GUQUET <cguquet@synexie.fr>
2023-12-03 23:44:10 +0100

    Day 1 commit

---
+++ Day01_01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Day01_01;
+
+public class Program
+{
+	public static void Main()
+	{
+        int total = 0;
+		var lines = File.ReadAllLines("input.txt");
+
+        foreach (var line in lines)
+        {
+            List<int> digits = new();
+
+            for (var i = 0 ; i < line.Length ; i++)
+            {
+                char c = line[i];
+                if (c >= '0' && c <= '9')
+                {
+                    var digit = int.Parse(c.ToString());
+                    digits.Add(digit);
+                }
+            }
+
+            var first = digits.First();
+            var last = digits.Last();
+
+            var number = int.Parse(first.ToString() + last.ToString());
+
+            total += number;
+        }
+
+        Console.WriteLine(total);
+	}
+}
---
+++ Day01_02/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Day01;
+
+public class Program
+{
+	public static void Main()
+	{
+        Dictionary<string, int> words = new()
+        {
+            {"one", 1},
+            {"two", 2},
+            {"three", 3},
+            {"four", 4},
+            {"five", 5},
+            {"six", 6},
+            {"seven", 7},
+            {"eight", 8},
+            {"nine", 9},
+            {"1", 1},
+            {"2", 2},
+            {"3", 3},
+            {"4", 4},
+            {"5", 5},
+            {"6", 6},
+            {"7", 7},
+            {"8", 8},
+            {"9", 9}
+        };
+
+        int total = 0;
+		var lines = File.ReadAllLines("input.txt");
+
+        foreach (var line in lines)
+        {
+            int? first = null;
+            int? last = null;
+
+            for (var i = 0 ; i < line.Length ; i++)
+            {
+                foreach(var key in words.Keys)
+                {
+                    var currentSubstring = line.Substring(i);
+                    if (currentSubstring.IndexOf(key) == 0)
+                    {                     
+                        var currentValue = words[key];
+
+                        if (first == null)
+                        {
+                            first = currentValue;
+                        }
+                        else
+                        {
+                            last = currentValue;
+                        }
+                    }
+                }                
+            }
+
+            var lineTotalWord = first.Value.ToString();
+            if (last.HasValue) lineTotalWord += last.Value.ToString();
+            else lineTotalWord += first.Value.ToString();
+            var lineTotal = int.Parse(lineTotalWord);
+
+            total += lineTotal;
+        }
+
+        Console.WriteLine(total);
+	}
+}
