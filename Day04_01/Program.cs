1112ac2a14f2a9d43ed92c3718a0801c8d5befab
Calendau GUQUET <cguquet@synexie.fr>
2023-12-05 12:51:08 +0100

    d4p1

---
+++ Day04_01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace Day04_01;
+
+public class Program
+{
+    public static void Main()
+    {
+        string[] lines = File.ReadAllLines("input.txt");
+
+        int total = 0;
+
+        foreach (var line in lines)
+        {
+            string[] cardParts = line.Split(':')[1].Split('|');
+
+            var winningNumbers = cardParts[0].Split(' ', StringSplitOptions.RemoveEmptyEntries).Where(x => !string.IsNullOrEmpty(x)).Select(x => int.Parse(x)).ToList();
+            var cardNumbers = cardParts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Where(x => !string.IsNullOrEmpty(x)).Select(x => int.Parse(x)).ToList();
+
+            var matches = cardNumbers.Intersect(winningNumbers).ToList();
+
+            total += (int)Math.Pow(2, matches.Count - 1);
+        }
+
+        Console.WriteLine(total);
+    }
+}
